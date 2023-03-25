9b27e795a1f96e5c5f6f8035ff0879c7bd8ee911
Andrii Ostin <ostin.andrew1@gmail.com>
2023-03-26 01:37:29 +0200

    added some styles

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,12 @@ import {
   Header,
 } from "./containers";
 import { CTA, Brand, Navbar } from "./Components";
+import './App.css'
 
 const App = () => {
   return (
     <div className="App">
-      <div className="gradient_bg">
+      <div className="gradient__bg">
         <Navbar />
         <Header />
       </div>
